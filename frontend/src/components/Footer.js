40b96d49392be7f62a22b9a6da1ec063cb65f351
hammadasdfg6-glitch <hammadasdfg6@gmail.com>
2026-08-23 10:22:27 +0500

    Keep only backend files in frontend-v-2 branch

--- frontend/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer fade-in">
-      <p>&copy; {new Date().getFullYear()} VestAuth. All rights reserved.</p>
-    </footer>
-  );
-}
